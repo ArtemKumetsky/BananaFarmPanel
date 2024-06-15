0c19a65ad198d5329f9de84e6aec48721577c9bf
Farmer <kumetsky2014@gmail.com>
2024-06-15 18:49:34 +0300

    Added license check on launching panel. Added License keys support

--- main.py
+++ main.py
@@ -45,13 +45,27 @@ def is_hwid_allowed(hwid):
     return doc.exists
 
 
+def product_key_check():
+    product_key = input("If you have a product key, please enter it: ").strip()
+    doc_ref = db.collection('product_keys').document(product_key)
+    doc = doc_ref.get()
+    if doc.exists:
+        print("Product key is valid! Saving your HWID to allowed list...")
+        db.collection('allowed_hwids').document(getHWID()).set({product_key: True})
+        db.collection('product_keys').document(product_key).delete()
+    else:
+        print("Wrong product key!")
+
+
 def checkHWID():
     hwid = getHWID()
     if is_hwid_allowed(hwid):
         print("HWID successfully checked! Launching program...")
     else:
+
         print("Error: HWID is unregistered. Please contact TG @jabablet !")
         print("Your current HWID: " + hwid)
+        product_key_check()
         time.sleep(30)
         exit(1)
 
@@ -61,24 +75,35 @@ checkHWID()
 
 # read accounts data from logpass.txt and add them to temporary array
 def read_accounts(file_path):
-    accounts = []
-    with open(file_path, 'r') as f:
-        for line in f:
-            login, password = line.strip().split(':')
-            accounts.append({"login": login, "password": password})
+    try:
+        accounts = []
+        with open(file_path, 'r') as f:
+            for line in f:
+                login, password = line.strip().split(':')
+                accounts.append({"login": login, "password": password})
 
-    return accounts
+        return accounts
+    except Exception as e:
+        print("Check your logpass.txt! Error:" + str(e))
+        logging.error("Check your logpass.txt! Error:" + str(e))
+        time.sleep(60)
 
 
 accounts = read_accounts('logpass.txt')
 
 # get mafiles
 for account in accounts:
-    account["mafile_path"] = f"mafiles/{account['login']}.mafile"
+    try:
+        account["mafile_path"] = f"mafiles/{account['login']}.mafile"
+    except Exception as e:
+        print("Check your mafiles! It will be renamed to accountLogin.mafile! Error: " + str(e))
+        logging.error("Check your mafiles! It will be renamed to accountLogin.mafile! Error: " + str(e))
+        time.sleep(60)
 
 # Steam path
 with open('settings.json', 'r') as f:
     config = json.load(f)
+
 
 steam_path = config['steam_path']
 game_id = "2923300"
@@ -97,26 +122,31 @@ def get_steam_guard_code(mafile_path):
     return authenticator.get_code()
 
 
-def find(image_path, timeout):
+def find(image_path, timeout, action):
     print(f"looking for {image_path}...")
     try:
         location = pyautogui.locateOnScreen(image_path, timeout)
-        if location is not None:
+        if location is not None and action == 1:
             pyautogui.click(pyautogui.center(location))
             print(f"{image_path} found! Closing...")
+        if location is not None and action == 0:
+            pyautogui.center(location)
+            print(f"{image_path} found!")
     except Exception as e:
         print(f"{image_path} not detected! {e}")
 
 
-def delayed_kill(pid,login):
-    print("Termination timer started, acc will be killed in " + str(terminate_timer) + " seconds")
-    time.sleep(terminate_timer)
+def delayed_kill(pid, login):
+    print("Termination timer started, acc will be killed in " + str(terminate_timer) + " seconds\n")
+    if terminate_timer > 0:
+        time.sleep(terminate_timer)
     subprocess.run(["taskkill", "/F", "/PID", str(pid)], check=True)
     print(f"{login} termination done!")
 
 
 def login_and_launch_game(account):
     try:
+        print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
         print(f"Launching {account['login']}")
         # get SDA code
         auth_code = get_steam_guard_code(account["mafile_path"])
@@ -126,7 +156,7 @@ def login_and_launch_game(account):
         process_ids[account['login']] = steam_window.pid
 
         # wait till steam launched
-        pyautogui.locateOnScreen("assets/login/steam_launched.png", 300)
+        find("assets/login/steam_launched.png", 300, 0)
         print(f"{account['login']}: Steam launched!")
 
         # enter login
@@ -136,34 +166,34 @@ def login_and_launch_game(account):
         # enter password
         pyautogui.write(account["password"])
         pyautogui.press("enter")
-        pyautogui.locateOnScreen("assets/login/sda_request.png", 300)
+        find("assets/login/sda_request.png", 300, 0)
 
         # enter SDA code
         pyautogui.write(auth_code)
         pyautogui.press("enter")
 
         # check game ban notification
-        find("assets/notifications/game_ban_window_close.png", 10)
+        find("assets/notifications/game_ban_window_close.png", 10, 1)
 
         # check EULA
-        find("assets/notifications/accept_eula.png", 10)
+        find("assets/notifications/accept_eula.png", 10, 1)
 
         # continue
         pyautogui.locateOnScreen("assets/login/steam_logged_in.png", 300)
         print(f"{account['login']}: Logged into account!")
 
         # check cloud sync fail notification
-        find("assets/notifications/sync_failed.png", 5)
+        find("assets/notifications/sync_failed.png", 5, 1)
 
         # close steam window
-        find("assets/close_steam_window.png", 30)
+        find("assets/close_steam_window.png", 30, 1)
 
         # Check game launch
-        pyautogui.locateOnScreen("assets/game_launched.png", 300)
+        find("assets/game_launched.png", 300, 0)
         print(f"{account['login']}: Game launched!")
 
         # minimize game
-        find("assets/minimize_game.png", 300)
+        find("assets/minimize_game.png", 300, 1)
 
         # start terminate timer if it enabled in settings.json
         if terminate_timer > 0:
@@ -175,10 +205,11 @@ def login_and_launch_game(account):
 while True:
     for account in accounts:
         try:
+            print("--------------------------------------------------------------------")
             login_and_launch_game(account)
             counter = counter + 1
-            print("\naccounts launched: " + str(counter))
-            time.sleep(3)
+            print("--------------------------------------------------------------------")
+            print("accounts launched: " + str(counter))
         except Exception as e:
             logging.error(f"Error in main cycle: {e}")
 
